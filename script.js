bb407bef402acab356ed51ec555840d88a152111
Raphaelly Santos <raphaelly.santos@rebase.com.br>
2024-11-20 11:25:07 -0300

    Color square on mouseover

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const mainContainer = document.querySelector("#main-container")
 
-
 for(var i = 0; i < 256; i++) {
-  const line = document.createElement("div")
-  line.style = "background-color: green; border-style: solid; height: 20px; width: 20px"
-  mainContainer.appendChild(line)
+  const square = document.createElement("div")
+  square.classList.add("square")
+  square.onmouseover = () => square.classList.add("colored-square");
+  mainContainer.appendChild(square)
 }
