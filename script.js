e467251126ed8a81261177f3aa5d3508bf2f8c04
Raphaelly Santos <raphaelly.santos@rebase.com.br>
2024-11-20 13:44:04 -0300

    Adjust grid container cleaning

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const mainContainer = document.querySelector("#main-container");
 const newGridButton = document.querySelector("#new-grid-button");
+const mainContainer = document.querySelector('#main-container');
 
 newGridButton.addEventListener("click", () => {
   let squaresPerSide = Number(prompt("Number of squares per side? (max: 100)"));
 
   if (isNaN(squaresPerSide)) {
-    return alert(`The chosen value is not a number. Please, try again.`)
-  }
+    return alert(`The chosen value is not a number. Please, try again.`);
+  };
   
   if (squaresPerSide > 100) {
-    return alert(`The chosen value is not under 100. Please, try again.`)
-  }
+    return alert(`The chosen value is not under 100. Please, try again.`);
+  };
 
-  const lines = document.querySelectorAll(".line")
-  lines.forEach((line) => line.remove());
+  const previousGridContainer = document.querySelector("#grid-container");
+  previousGridContainer?.remove();
+  
+  const gridContainer = document.createElement("div");
+  gridContainer.id = "grid-container";
+  mainContainer.appendChild(gridContainer);
 
   let size = 500/squaresPerSide;
 
@@ -21,7 +25,7 @@ newGridButton.addEventListener("click", () => {
     const line = document.createElement("div");
     line.classList.add("line")
     line.style = "display: flex; justify-content: center";
-    mainContainer.appendChild(line);
+    gridContainer.appendChild(line);
 
     for(let i = 0; i < squaresPerSide; i++) {
       const square = document.createElement("div");
