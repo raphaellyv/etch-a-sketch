fe499b72310c2ef231a6b9a28e6265031f7349f5
Raphaelly Santos <raphaelly.santos@rebase.com.br>
2024-11-20 20:15:13 -0300

    Add image title and adjust alerts

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ const mainContainer = document.querySelector('#main-container');
 newGridButton.addEventListener("click", () => {
   let squaresPerSide = Number(prompt("Number of squares per side? (max: 100)"));
 
-  if (isNaN(squaresPerSide)) {
-    return alert(`The chosen value is not a number. Please, try again.`);
-  };
-  
-  if (squaresPerSide > 100) {
-    return alert(`The chosen value is not under 100. Please, try again.`);
+  if (isNaN(squaresPerSide) || squaresPerSide > 100 || squaresPerSide <= 0) {
+    return alert(`The chosen value is not a valid number. Please, try again.`);
   };
 
   const previousGridContainer = document.querySelector("#grid-container");
@@ -19,18 +15,17 @@ newGridButton.addEventListener("click", () => {
   gridContainer.id = "grid-container";
   mainContainer.appendChild(gridContainer);
 
-  let size = 500/squaresPerSide;
+  let squareSize = 500/squaresPerSide;
 
   for(let i = 0; i < squaresPerSide; i++) {
     const line = document.createElement("div");
     line.classList.add("line")
-    line.style = "display: flex; justify-content: center";
     gridContainer.appendChild(line);
 
     for(let i = 0; i < squaresPerSide; i++) {
       const square = document.createElement("div");
       square.classList.add("square");
-      square.style = `height: ${size}px; width: ${size}px`;
+      square.style = `height: ${squareSize}px; width: ${squareSize}px`;
       square.onmouseover = () => square.classList.add("colored-square");
       line.appendChild(square);
     };
